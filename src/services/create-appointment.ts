105fdbe683b1b511a25262b3d3c42eb55c0d03b3
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-07 23:57:42 -0300

    feat(appointment): Create a service to create an appointment

---
+++ src/services/create-appointment.ts
@@ -0,0 +1,23 @@
+import { Appointment } from "../entities/appointment";
+
+interface CreateAppointmentRequest {
+    customer: string;
+    startsAt: Date;
+    endsAt: Date;
+}
+
+type CreateAppointmentResponse = Appointment;
+
+export class createAppointment {
+     async execute({
+        customer,
+        startsAt,
+        endsAt
+    }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+        return new Appointment({
+            customer,
+            startsAt,
+            endsAt
+        });
+    }
+}
