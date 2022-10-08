f33c21e39ec2618bd0235174360b767fd492dee7
Pedro Cunha <pedro.cunha@onfinity.com.br>
2022-10-07 23:25:36 -0300

    feat(appointment): Create simple class

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -1,3 +1,31 @@
-class Appointment {
+export interface AppointmentProps {
+    customer: string;
+    startsAt: Date;
+    endsAt: Date;
+}
 
+export class Appointment {
+    private props: AppointmentProps;
+
+    get customer() {
+        return this.props.customer;
+    }
+
+    get startsAt() {
+        return this.props.startsAt;
+    }
+
+    get endsAt() {
+        return this.props.endsAt;
+    }
+
+    constructor(props: AppointmentProps) {
+
+        const { startsAt, endsAt } = props;
+
+        if(startsAt >= endsAt) {
+            throw new Error("Start date must be before end date");
+        }
+        this.props = props;
+    }
 }
